1b24925bbe1c3199a46cf655f9daede99880fe95
Your Name <you@example.com>
2025-07-05 15:07:20 +0100

    build: Configure Next.js

---
+++ frontend/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://localhost:3002/api/v1/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
